2c209a8fa777225208b6d7c947e6398be49953ca
CodeRacon <michael.buschmann.86@gmail.com>
2024-05-26 20:56:38 +0200

    feat: implemented + styled 'footer' component, in 'shared' - sub-folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './main-content/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MainContentComponent],
+  imports: [RouterOutlet, CommonModule, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
