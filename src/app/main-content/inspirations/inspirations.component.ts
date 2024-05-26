c6d410609d307b29ace96826e1b5c0c259989a1a
CodeRacon <michael.buschmann.86@gmail.com>
2024-05-26 20:44:02 +0200

    feat: implemented + styled 'inspirations' component, nested in 'main-content' - component

---
+++ src/app/main-content/inspirations/inspirations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspirations',
+  standalone: true,
+  imports: [],
+  templateUrl: './inspirations.component.html',
+  styleUrl: './inspirations.component.scss'
+})
+export class InspirationsComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { PhilosophyComponent } from './philosophy/philosophy.component';
 import { MaterialsComponent } from './materials/materials.component';
+import { InspirationsComponent } from './inspirations/inspirations.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomeComponent, PhilosophyComponent, MaterialsComponent],
+  imports: [
+    HomeComponent,
+    PhilosophyComponent,
+    MaterialsComponent,
+    InspirationsComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
